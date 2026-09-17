9614d88d5208cdea5da650f1ab5c1822cbbfdacf
Joshua Falco Beirer <joshua.beirer@cern.ch>
2026-09-17 14:05:14 +0200

    Bump to 0.6.0

--- src/rootfig/__init__.py
+++ src/rootfig/__init__.py
@@ -53,7 +53,7 @@ from rootfig.histograms import (
 from rootfig.model import Cut, Sample, Style, Systematic, Variable, log_bins
 from rootfig.plotting import Plot, dark_theme, use_style
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
 __all__ = [
     "BinningError",
